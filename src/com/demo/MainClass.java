e31b142b0ff3cabcd78472411032515a14dac6e5
naikcharan24 <naikcharan24@gmail.com>
2023-10-12 15:20:03 +0530

    GitHost1 commit 1

---
+++ src/com/demo/MainClass.java
@@ -0,0 +1,15 @@
+package com.demo;
+
+import com.demo.pack1.Class1;
+
+public class MainClass
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Main method is started");
+		Class1.method1();
+		System.out.println("Main method is ended");
+	}
+}
+
+// https://github.com/naikcharan24/GitHost1.git
---
+++ src/com/demo/pack1/Class1.java
@@ -0,0 +1,9 @@
+package com.demo.pack1;
+
+public class Class1
+{
+	public static void method1()
+	{
+		System.out.println("method1 has staeted");
+	}
+}
